bb79ad2ea0f20a9539ea63f56e220584834fcc77
Moritz Wolf <moritz.wolf@desy.de>
2024-10-14 14:48:11 +0200

    add equality constraint with adaptive epsilon

--- my_mdmm.py
+++ my_mdmm.py
@@ -34,12 +34,12 @@ class Constraint(nn.Module, metaclass=abc.ABCMeta):
         return f'scale={self.scale:g}, damping={self.damping:g}'
 
     @abc.abstractmethod
-    def infeasibility(self, fn_value):
+    def infeasibility(self, fn_value, allow_adaptive_epsilon=False):
         ...
 
-    def forward(self, inp, out, target):
+    def forward(self, inp, out, target, allow_adaptive_epsilon=False):
         fn_value = self.fn(inp, out, target)
-        inf = self.infeasibility(fn_value)
+        inf = self.infeasibility(fn_value, allow_adaptive_epsilon)
         l_term = self.lmbda * inf
         damp_term = self.damping * inf**2 / 2
 
@@ -63,7 +63,49 @@ class EqConstraint(Constraint):
     def extra_repr(self):
         return f'value={self.value:g}, scale={self.scale:g}, damping={self.damping:g}'
 
-    def infeasibility(self, fn_value):
+    def infeasibility(self, fn_value, allow_adaptive_epsilon=False):
+        return self.value - fn_value
+
+
+class EqConstraintAdaptiveEpsilon(Constraint):
+    """Represents an equality constraint."""
+
+    def __init__(self, fn, value, scale=1., damping=1., lmbda_init=0., look_back=100):
+        super().__init__(fn, scale, damping, lmbda_init)
+        self.register_buffer('value', torch.as_tensor(value))
+
+        self.register_buffer('look_back', torch.as_tensor(look_back))
+        self.register_buffer('last_fn_values', torch.empty((0)))
+
+    def extra_repr(self):
+        return f'value={self.value:g}, scale={self.scale:g}, damping={self.damping:g}'
+
+    def infeasibility(self, fn_value, allow_adaptive_epsilon=False):
+
+        if allow_adaptive_epsilon:
+            with torch.no_grad():
+
+                if self.last_fn_values is not None:
+
+                    if not self.last_fn_values.device == fn_value.device:
+                        self.last_fn_values = self.last_fn_values.to(fn_value.device)
+
+                    self.last_fn_values = torch.cat((self.last_fn_values, fn_value.unsqueeze(0)))
+
+                    if self.last_fn_values.size()[0] >= 3 * self.look_back:
+                        if self.last_fn_values[-self.look_back:].mean() \
+                                > self.last_fn_values[-2*self.look_back:-self.look_back].mean() \
+                                > self.last_fn_values[-3*self.look_back:-2*self.look_back].mean():
+                            if self.last_fn_values[-3*self.look_back:-2*self.look_back].mean() < self.value:
+                                self.value = self.last_fn_values[-3*self.look_back:-2*self.look_back].mean()
+                                self.last_fn_values = None
+                                print('epsilon adapted to ' + str(self.value.item()))
+
+                    # if self.last_fn_values.size()[0] >= self.look_back and self.last_fn_values[-self.look_back:].mean() < self.value:
+                    #     self.value = self.last_fn_values[-self.look_back:].mean()
+                    #     self.last_fn_values = torch.empty((0))
+                    #     print('epsilon adapted to ' + str(self.value.item()))
+
         return self.value - fn_value
 
 
@@ -78,7 +120,7 @@ class MaxConstraint(Constraint):
     def extra_repr(self):
         return f'max={self.max:g}, scale={self.scale:g}, damping={self.damping:g}'
 
-    def infeasibility(self, fn_value):
+    def infeasibility(self, fn_value, allow_adaptive_epsilon=False):
         if self.slack.isnan():
             with torch.no_grad():
                 self.slack.copy_((self.max - fn_value).relu().pow(1/2))
@@ -95,7 +137,7 @@ class MaxConstraintHard(Constraint):
     def extra_repr(self):
         return f'max={self.max:g}, scale={self.scale:g}, damping={self.damping:g}'
 
-    def infeasibility(self, fn_value):
+    def infeasibility(self, fn_value, allow_adaptive_epsilon=False):
         return fn_value.clamp(max=self.max) - fn_value
 
 
@@ -110,7 +152,7 @@ class MinConstraint(Constraint):
     def extra_repr(self):
         return f'min={self.min:g}, scale={self.scale:g}, damping={self.damping:g}'
 
-    def infeasibility(self, fn_value):
+    def infeasibility(self, fn_value, allow_adaptive_epsilon=False):
         if self.slack.isnan():
             with torch.no_grad():
                 self.slack.copy_((fn_value - self.min).relu().pow(1/2))
@@ -127,7 +169,7 @@ class MinConstraintHard(Constraint):
     def extra_repr(self):
         return f'min={self.min:g}, scale={self.scale:g}, damping={self.damping:g}'
 
-    def infeasibility(self, fn_value):
+    def infeasibility(self, fn_value, allow_adaptive_epsilon=False):
         return fn_value.clamp(min=self.min) - fn_value
 
 
@@ -143,7 +185,7 @@ class BoundConstraintHard(Constraint):
         return f'min={self.min:g}, max={self.max:g}, ' \
                f'scale={self.scale:g}, damping={self.damping:g}'
 
-    def infeasibility(self, fn_value):
+    def infeasibility(self, fn_value, allow_adaptive_epsilon=False):
         return fn_value.clamp(self.min, self.max) - fn_value
 
 
@@ -165,11 +207,11 @@ class MDMM(nn.ModuleList):
                           {'params': lambdas, 'lr': -lr_lambda_factor*lr},
                           {'params': slacks, 'lr': lr}])
 
-    def forward(self, loss, inp, out, target, switch_off_constraints=False):
+    def forward(self, loss, inp, out, target, switch_off_constraints=False, allow_adaptive_epsilon=False):
         value = loss.clone()
         fn_values, infs = [], []
         for c in self:
-            c_return = c(inp, out, target)
+            c_return = c(inp, out, target, allow_adaptive_epsilon)
             if not switch_off_constraints: value += c_return.value
             fn_values.append(c_return.fn_value)
             infs.append(c_return.inf)
